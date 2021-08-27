3045bd3dab5dd52dcad3a2acc61b3ebf6b5dc292
Carlos <carlos96831@gmail.com>
2021-08-27 01:06:46 -0700

    worked on Styles sheet

---
+++ script.js
@@ -0,0 +1,20 @@
+var Questions = [
+{
+    question: "What is 2+2?",
+    answers: {
+    a:'fish',
+    b: 'potatoes',
+    c: '4'
+},
+   correctAnswer: '4' 
+},
+{
+    question: "What is 30*3?",
+    answers: {
+    a:'fish',
+    b: 'potatoes',
+    c: '90'
+},
+   correctAnswer: '90' 
+}
+];
