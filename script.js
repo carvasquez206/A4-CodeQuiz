00c776e35f4a29d2f8c43afdf9fead9200f6308e
Carlos <carlos96831@gmail.com>
2021-08-28 21:03:24 -0700

    messed around with JavaScript

--- script.js
+++ script.js
@@ -1,22 +1,102 @@
 const data = [
-{
-    question: "What is 2+2?",
-    answers: {
-    a:'fish',
-    b: 'potatoes',
-    c: '4'
-},
-   correctAnswer: '4' 
-},
-{
-    question: "What is 30*3?",
-    answers: {
-    a:'fish',
-    b: 'potatoes',
-    c: '90'
-},
-   correctAnswer: '90' 
-}
+    {
+        question: "Who is Mike Jones?",
+        a:'who',
+        b: 'who',
+        c: 'Mike Jones',
+        d: 'President of the US',
+        correct: 'Mike Jones', 
+    },
+    {
+        question: "What is 30*3?",
+        a:'fish',
+        b: 'potatoes',
+        c: '90',
+        d: '4',
+        correct: "a",
+    },
+    {
+        question: "What is 2+2?",
+        a:'fish',
+        b: 'potatoes',
+        c: '4',
+        d: '4',
+        correct: '4', 
+    },
+    {
+        question: "What is 2+2?",
+        a:'fish',
+        b: 'potatoes',
+        c: '4',
+        d: '4',
+        correct: '4', 
+    },
 ]
 
-const quiz
+/* good from here on*/
+const quiz = document.getElementById("Quiz")
+const uestionE1s = document.querySelectorAll(".answer")
+const questionE1 = document.getElementById("question")
+const optionA = document.getElementById("optionA")
+const optionB = document.getElementById("optionB")
+const optionC = document.getElementById("optionC")
+const optionD = document.getElementById("optionD")
+
+const submitBtn = document.getElementById("submit")
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+/*so that text changes */
+function loadQuiz(){
+    deselectAnswers()
+
+    const currentQuizData = data[currentQuiz]
+
+    questionE1.innerText = currentQuizData.question
+    
+    optionA.innerText = currentQuizData.a
+    optionB.innerText = currentQuizData.b
+    optionC.innerText = currentQuizData.c
+    optionD.innerText = currentQuizData.d 
+}
+
+function deselectAnswers(){
+    answerE1S.forEach((answerE1) => {
+        answerE1.checked = false
+    })
+}
+
+funtion getSelect(){
+    let answer
+
+    answerE1S.forEach((answerE1) => {
+        if(answerE1.checked){
+        answer = answerE1.id
+        }
+    })
+
+    return answer
+}
+
+submitBtn.addEventListener("click"), () => {
+    const answer = getSelected()
+
+    if(answer) {
+        if(answer===data[currentQuiz].correct){
+            score++
+        }
+        currentQuiz++
+        if(currentQuiz < data.length) {
+            loadQuiz
+        }
+        else {
+            quiz.innerHTML = `
+            
+            <h2>You Answered ${score}/${data.legth} Questions Correctly</h2>
+            <button onclick="loaction.reload()">Do it Again</button>`
+        }
+    }       
+})
